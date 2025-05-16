2dab62a38513e4c1ed50f009e978e87ce52d5bfe
EngineersBox <35655145+EngineersBox@users.noreply.github.com>
2025-05-16 20:54:38 +1000

    Alternate memset
    Signed-off-by: EngineersBox <35655145+EngineersBox@users.noreply.github.com>

--- libpsn00b/libc/memset.s
+++ libpsn00b/libc/memset.s
@@ -1,119 +1,182 @@
 # PSn00bSDK optimized memset
 # (C) 2022 spicyjpeg - MPL licensed
 
-.set noreorder
+# Parameters:
+# $a0: destination pointer
+# $a1: byte chararcter to write
+# $a2: number of bytes to write starting from dest pointer
 
-.section .text.memset, "ax", @progbits
-.global memset
-.type memset, @function
+/*
+
+MIT License
+
+Copyright (c) 2020 PCSX-Redux authors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+    .section .ramtext, "ax", @progbits
+    .align 2
+    .global memset
+    .type memset, @function
+    .set noreorder
+
+/* void * memset(void * ptr, int value, size_t num); */
+/* http://man7.org/linux/man-pages/man3/memset.3.html */
 
 memset:
-	# If more than 16 bytes have to be written then take the "large" path,
-	# otherwise use the code below.
-	addiu $t0, $a2, -16
-	bgtz  $t0, .Llarge_fill
-	move  $v0, $a0 # return_value = dest
+    beqz    $a2, out
+    move    $v0, $a0
 
-	# Jump to one of the sb opcodes below. This is basically a cut-down Duff's
-	# device implementation with no looping.
-	la    $t0, .Lsmall_duff + 0x40 # jump_addr = &small_duff[(16 - count) * 4]
-	sll   $t1, $a2, 2
-	subu  $t0, $t1
-	addu  $a0, $a2 # dest -= 16 - count
-	jr    $t0
-	addiu $a0, -16
+    sltiu   $v1, $a2, 32
+    beqz    $v1, large_enough
+    move    $t0, $a1
 
-.Lsmall_duff:
-	sb    $a1, 0x0($a0)
-	sb    $a1, 0x1($a0)
-	sb    $a1, 0x2($a0)
-	sb    $a1, 0x3($a0)
-	sb    $a1, 0x4($a0)
-	sb    $a1, 0x5($a0)
-	sb    $a1, 0x6($a0)
-	sb    $a1, 0x7($a0)
-	sb    $a1, 0x8($a0)
-	sb    $a1, 0x9($a0)
-	sb    $a1, 0xa($a0)
-	sb    $a1, 0xb($a0)
-	sb    $a1, 0xc($a0)
-	sb    $a1, 0xd($a0)
-	sb    $a1, 0xe($a0)
-	sb    $a1, 0xf($a0)
-	jr    $ra
-	nop
+    addu    $a2, $a0
+    addiu   $a2, -1
+small_memset_loop:
+    sb		$a1, 0($a0)
+    bne     $a0, $a2, small_memset_loop
+    addiu   $a0, 1
+    jr      $ra
 
-.Llarge_fill:
-	# Initialize fast filling by repeating the fill byte 4 times, so it can be
-	# written 32 bits at a time.
-	andi  $a1, 0xff # ch &= 0xff
-	sll   $t0, $a1, 8 # ch |= (ch << 8) | (ch << 16) | (ch << 24)
-	or    $a1, $t0
-	sll   $t0, $a1, 16
-	or    $a1, $t0
+large_enough:
+    sll     $t0, 8
+    or      $t0, $a1
+    move    $a1, $t0
+    sll     $t0, 16
+    or      $a1, $t0
 
-	# Fill the first 1-4 bytes (here the swr instruction does all the magic)
-	# and update dest and count accordingly.
-	swr   $a1, 0($a0)
-	andi  $t0, $a0, 3 # align = 4 - (dest % 4)
-	addiu $t0, -4
-	addu  $a2, $t0 # count -= align
-	subu  $a0, $t0 # dest += align
+    andi    $v1, $a0, 3
+    la      $t1, jumptable1
+    sll     $t2, $v1, 2
+    subu    $a2, $v1
+    addu    $t2, $t1
+    lw      $t1, 0($t2)
+    srl     $t0, $a2, 8
+    jr      $t1
+    andi    $t1, $a2, 0xff
 
-	la    $t1, .Llarge_duff
-	andi  $t2, $a2, 3 # remainder = count % 4
-	subu  $a2, $t2 # count -= remainder
+jumptable1:
+    .word   sb0
+    .word   sb3
+    .word   sb2
+    .word   sb1
 
-.Llarge_fill_loop:
-	# If 128 bytes or more still have to be written, skip calculating the jump
-	# offset and execute the whole block of sw opcodes.
-	addiu $a2, -0x80 # count -= 0x80
-	bgez  $a2, .Llarge_duff
-	#nop
+sb3:
+    sb      $a1, 0($a0)
+    addiu   $a0, 1
+sb2:
+    sb      $a1, 0($a0)
+    addiu   $a0, 1
+sb1:
+    sb      $a1, 0($a0)
+    addiu   $a0, 1
+sb0:
 
-	# Jump to one of the sw opcodes below. This is the "full" Duff's device.
-	subu  $t0, $t1, $a2 # jump_addr = &large_duff[0x80 - (count + 0x80)]
-	jr    $t0
-	addu  $a0, $a2 # dest -= 0x80 - (count + 0x80)
+/* At this point, we have:
+v0 - our return value
+a0 - current, aligned pointer
+a1 - our word to store
+t0 - our big loop counter
+t1 - the remainder counter to store
+*/
 
-.Llarge_duff:
-	sw    $a1, 0x00($a0)
-	sw    $a1, 0x04($a0)
-	sw    $a1, 0x08($a0)
-	sw    $a1, 0x0c($a0)
-	sw    $a1, 0x10($a0)
-	sw    $a1, 0x14($a0)
-	sw    $a1, 0x18($a0)
-	sw    $a1, 0x1c($a0)
-	sw    $a1, 0x20($a0)
-	sw    $a1, 0x24($a0)
-	sw    $a1, 0x28($a0)
-	sw    $a1, 0x2c($a0)
-	sw    $a1, 0x30($a0)
-	sw    $a1, 0x34($a0)
-	sw    $a1, 0x38($a0)
-	sw    $a1, 0x3c($a0)
-	sw    $a1, 0x40($a0)
-	sw    $a1, 0x44($a0)
-	sw    $a1, 0x48($a0)
-	sw    $a1, 0x4c($a0)
-	sw    $a1, 0x50($a0)
-	sw    $a1, 0x54($a0)
-	sw    $a1, 0x58($a0)
-	sw    $a1, 0x5c($a0)
-	sw    $a1, 0x60($a0)
-	sw    $a1, 0x64($a0)
-	sw    $a1, 0x68($a0)
-	sw    $a1, 0x6c($a0)
-	sw    $a1, 0x70($a0)
-	sw    $a1, 0x74($a0)
-	sw    $a1, 0x78($a0)
-	sw    $a1, 0x7c($a0)
+    beqz    $t0, skip_big_loop
 
-	bgtz  $a2, .Llarge_fill_loop
-	addiu $a0, 0x80 # dest += 0x80
+big_loop:
+    addiu   $t0, -1
+    sw		$a1, 0x0000($a0)
+    sw		$a1, 0x0004($a0)
+    sw		$a1, 0x0008($a0)
+    sw		$a1, 0x000c($a0)
+    sw		$a1, 0x0010($a0)
+    sw		$a1, 0x0014($a0)
+    sw		$a1, 0x0018($a0)
+    sw		$a1, 0x001c($a0)
+    sw		$a1, 0x0020($a0)
+    sw		$a1, 0x0024($a0)
+    sw		$a1, 0x0028($a0)
+    sw		$a1, 0x002c($a0)
+    sw		$a1, 0x0030($a0)
+    sw		$a1, 0x0034($a0)
+    sw		$a1, 0x0038($a0)
+    sw		$a1, 0x003c($a0)
+    sw		$a1, 0x0040($a0)
+    sw		$a1, 0x0044($a0)
+    sw		$a1, 0x0048($a0)
+    sw		$a1, 0x004c($a0)
+    sw		$a1, 0x0050($a0)
+    sw		$a1, 0x0054($a0)
+    sw		$a1, 0x0058($a0)
+    sw		$a1, 0x005c($a0)
+    sw		$a1, 0x0060($a0)
+    sw		$a1, 0x0064($a0)
+    sw		$a1, 0x0068($a0)
+    sw		$a1, 0x006c($a0)
+    sw		$a1, 0x0070($a0)
+    sw		$a1, 0x0074($a0)
+    sw		$a1, 0x0078($a0)
+    sw		$a1, 0x007c($a0)
+    sw		$a1, 0x0080($a0)
+    sw		$a1, 0x0084($a0)
+    sw		$a1, 0x0088($a0)
+    sw		$a1, 0x008c($a0)
+    sw		$a1, 0x0090($a0)
+    sw		$a1, 0x0094($a0)
+    sw		$a1, 0x0098($a0)
+    sw		$a1, 0x009c($a0)
+    sw		$a1, 0x00a0($a0)
+    sw		$a1, 0x00a4($a0)
+    sw		$a1, 0x00a8($a0)
+    sw		$a1, 0x00ac($a0)
+    sw		$a1, 0x00b0($a0)
+    sw		$a1, 0x00b4($a0)
+    sw		$a1, 0x00b8($a0)
+    sw		$a1, 0x00bc($a0)
+    sw		$a1, 0x00c0($a0)
+    sw		$a1, 0x00c4($a0)
+    sw		$a1, 0x00c8($a0)
+    sw		$a1, 0x00cc($a0)
+    sw		$a1, 0x00d0($a0)
+    sw		$a1, 0x00d4($a0)
+    sw		$a1, 0x00d8($a0)
+    sw		$a1, 0x00dc($a0)
+    sw		$a1, 0x00e0($a0)
+    sw		$a1, 0x00e4($a0)
+    sw		$a1, 0x00e8($a0)
+    sw		$a1, 0x00ec($a0)
+    sw		$a1, 0x00f0($a0)
+    sw		$a1, 0x00f4($a0)
+    sw		$a1, 0x00f8($a0)
+    sw		$a1, 0x00fc($a0)
+    bnez    $t0, big_loop
+    addiu   $a0, 0x0100
 
-	# Fill the remaining 1-4 bytes, using (again) an unaligned store.
-	addu  $a0, $t2 # last_byte = dest + remainder - 1
-	jr    $ra
-	swl   $a1, -1($a0)
+skip_big_loop:
+    beqz    $t1, out
+
+    addu    $a2, $t1, $a0
+    b       small_memset_loop
+    addiu   $a2, -1
+
+out:
+    jr      $ra
+    nop
